51effbdd4e5d459b3cb93d0cdd2eab7009aa0bde
gs2589 <gabriel.seidner@gmail.com>
2016-07-17 12:19:01 -0400

    CtCI_4_1

--- CtCI_4_1.rb
+++ CtCI_4_1.rb
@@ -1,1 +1,22 @@
-#Initial Commit
+#CtCI 4.1
+
+
+
+def has_route?(node_1, node_2)
+    q= Queue.new()
+    node_1.visited=true
+    q.enqueue(node_1)
+    
+    while (!q.isEmpty?)
+        r=q.dequeue
+        for i in 0..r.adjacent.length
+           return true if r.adjacent[i]==node_2
+           if !r.adjacent[i].visited
+            q.enqueu(r.adjacent[i])
+            r.adjacent[i].visited=true
+           end 
+        end
+    end
+    
+    return false     
+end  
